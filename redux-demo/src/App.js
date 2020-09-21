e31d4e3c54f8eeb19fc3c49c9050d1bfc88fb1b1
Sabina Timsina <sabinatimsina18@gmail.com>
2020-09-21 10:29:51 +0545

    redux done

---
+++ redux-demo/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+// components
+
+// items
+
+// redux stuff
+
+import { createStore } from "redux";
+import reducer from "./reducer";
+// react-redux - Provider - wraps app , connect - used in components
+import { Provider } from "react-redux";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// initial store
+
+// store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar/>
+      <CartContainer/>
+    </Provider>
+  );
+}
+
+export default App;
